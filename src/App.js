5fb5b03f1df664480ed4df595c068e0d6f8cb1f8
NeetuKV <pranee6r@yahoo.com>
2023-02-03 12:51:03 -0800

    removed button

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <br />
-        <a href="#" className="btn btn-primary">
-          Button
-        </a>
       </header>
       Dictionary Project
     </div>
