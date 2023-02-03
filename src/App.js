ff55c0d77537f086565572cbcea85a6ba0f3d5c6
NeetuKV <pranee6r@yahoo.com>
2023-02-03 12:48:57 -0800

    added a button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <a href="#" className="btn btn-primary">
+          Button
+        </a>
       </header>
       Dictionary Project
     </div>
