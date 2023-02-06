7c41b8485077374e64ed95fbb1d5dd27888d90ca
NeetuKV <pranee6r@yahoo.com>
2023-02-06 09:34:54 -0800

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyWord} definition`);
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
+    console.log(apiURL);
+
+    axios.get(apiURL).then(handleResponse);
   }
   function handleKeyWordChange(event) {
     setKeyWord(event.target.value);
