de20a6063630212cb9a518d5ee7b08855e9efc6d
NeetuKV <pranee6r@yahoo.com>
2023-02-06 10:21:10 -0800

    added a meaning componenet

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
@@ -22,6 +25,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeyWordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
