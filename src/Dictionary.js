4360dd04b0c4d8e028fcf3b222dbb74e0b9ddc7b
NeetuKV <pranee6r@yahoo.com>
2023-02-08 10:08:22 -0800

    aded synonym component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,15 +6,11 @@ export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
   let [results, setResults] = useState(null);
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyWord} definition`);
     let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
-    console.log(apiURL);
-
     axios.get(apiURL).then(handleResponse);
   }
   function handleKeyWordChange(event) {
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   console.log(props.meaning);
 
@@ -10,9 +11,14 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definitions : </strong>
               {definition.definition}
               <br />
+              <strong>Example :</strong>
               <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
+              <strong>Synonyms : </strong>
+              {definition.synonyms}
             </p>
           </div>
         );
